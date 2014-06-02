2649ed4bc220f90d0653ebb2f4c8103371b49172
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-02 21:38:00 +0300

    minifix

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
-if (isset(getParams('eventId'))) // Get event from DB
-	Session.set('eventId', getParams('eventId'))
-else { // Create new event
+var eventId = getParams('eventId');
+if (isset(eventId))
+
+	// Get event from DB
+	Session.set('eventId', eventId);
+else {
+
+	// Create new event
 	Session.set('eventId', generateHash())
 	var defaultEvent = {
 		_id: Session.get('eventId'),
