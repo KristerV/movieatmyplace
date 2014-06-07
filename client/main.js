d668b899ce1dc25327925a2c31f310974d1d3dbb
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-07 10:31:06 +0300

    Faster initial loading

--- client/main.js
+++ client/main.js
@@ -3,35 +3,35 @@ if (!isset(localStorage.getItem("userId")))
 	localStorage.setItem("userId", generateHash());
 
 createDefaultEvent = function(eventId) {
-	var defaultEvent = {
+	Events.insert({
 		_id: eventId,
 		host: {
 			Name: '',
 			Location: '',
 			Time: '',
 		}
-	}
-
-	// Events collection isn't ready right away
-	Meteor.setTimeout(function(){
-		Events.insert(defaultEvent);
-	}, 100);
+	});
 }
 
 
-Meteor.startup(function () {    
-	Meteor.setTimeout(function(){
-		var eventId = getParams('eventId');
+Meteor.startup(function () {
+	count = 0;
+	var interval = Meteor.setInterval(function(){
+		count++;
+		if (typeof Events != 'undefined' && isset(Events.findOne())) {
+			var eventId = getParams('eventId');
 
-		if (!isset(eventId)) {
-			var eventId = generateHash();
-			createDefaultEvent(eventId);
-		} else if (!isset(Events.findOne({_id: eventId}))) {
-			// User is trying to make custom id - good!
-			createDefaultEvent(eventId);
+			if (!isset(eventId)) {
+				var eventId = generateHash();
+				createDefaultEvent(eventId);
+			} else if (!isset(Events.findOne({_id: eventId}))) {
+				// User is trying to make custom id - good!
+				createDefaultEvent(eventId);
+			}
+
+			Session.set('isLoading', false);
+			Session.set('eventId', eventId);
+			Meteor.clearInterval(interval);
 		}
-
-		Session.set('isLoading', false);
-		Session.set('eventId', eventId);
-	}, 1000);
+	}, 50);
 });
