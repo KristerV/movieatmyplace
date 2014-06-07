ab9444daa315bab62cb6ebe5be9a2d990203110b
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-07 09:38:54 +0300

    more universal delete command

--- client/controllers/popup.js
+++ client/controllers/popup.js
@@ -18,15 +18,17 @@ Template.editData.events({
 	'submit form': function(e, tmpl) {
 		e.preventDefault();
 		var buttonPressed = $(":input[type=submit]:focus").attr('name');
-		var dataPath = Session.get('editData')['dataPath'].join('.');
+		var dataPath = Session.get('editData')['dataPath'];
 		var dataId = Session.get('editData')['id'];
 		if (buttonPressed == 'save') {
 			var formData = getFormData('form[name="editDataForm"]');
 			var save = {};
-			save[dataPath] = formData;
+			save[dataPath.join('.')] = formData;
 			Events.update({_id: Session.get('eventId')}, {$set: save});
 		} else if (buttonPressed == 'delete') {
-			Events.update({_id: Session.get('eventId')}, {$pull: {films: {id: dataId}}});
+			var item = {};
+			item[dataPath[0]] = {id: dataId};
+			Events.update({_id: Session.get('eventId')}, {$pull: item});
 		}
 
 		Session.set('editData', null);
