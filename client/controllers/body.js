ad82771b97f2af8b972f4216d89e8287f96a06bf
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-08 12:47:55 +0300

    sexy cover image

--- client/controllers/body.js
+++ client/controllers/body.js
@@ -4,5 +4,12 @@ Template.body.helpers({
 	},
 	isPopup: function(){
 		return isset(Session.get('editData')) ? true : false;
+	},
+	youtubeImage: function() {
+		var url = 'https://www.youtube.com/watch?v=QJ-TwPHs-Ss';
+		var results = url.match("[\?&amp;]v=([^&amp;#]*)");
+		var vid = ( results === null ) ? url : results[1];
+		var image = "http://img.youtube.com/vi/"+vid+"/0.jpg";
+		return image;
 	}
 });
