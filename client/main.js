de6056802d3533ba1b518875c1c7dd0ca733e925
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-10 22:35:28 +0300

    commenting code

--- client/main.js
+++ client/main.js
@@ -1,11 +1,16 @@
+// Redirect to long url if short
 var url = getDomain();
 if (url[0].indexOf("movieat.mp") > -1)
 	window.location.replace("http://movieatmyplace.com/?" + url[1]);
 
+// Hide content while still loading site
 Session.setDefault('isLoading', true);
+
+// Generate userId if none
 if (!isset(localStorage.getItem("userId")))
 	localStorage.setItem("userId", generateHash());
 
+// An empty event with all necessary details
 createDefaultEvent = function(eventId) {
 	Events.insert({
 		_id: eventId,
@@ -18,23 +23,34 @@ createDefaultEvent = function(eventId) {
 }
 
 
+// When meteor starts up
 Meteor.startup(function () {
-	count = 0;
+
+	// Check if database is ready in interval
 	var interval = Meteor.setInterval(function(){
-		count++;
 		if (typeof Events != 'undefined' && isset(Events.findOne())) {
 			var eventId = getParams('eId');
 
 			if (!isset(eventId)) {
+
+				// If no event id in GET, generate one
 				var eventId = generateHash();
 				createDefaultEvent(eventId);
+
 			} else if (!isset(Events.findOne({_id: eventId}))) {
+
+				// EventId is in GET, but not in database
 				// User is trying to make custom id - good!
 				createDefaultEvent(eventId);
 			}
 
+			// Remove loading screen
 			Session.set('isLoading', false);
+
+			// Save eventid for later
 			Session.set('eId', eventId);
+
+			// Stop interval
 			Meteor.clearInterval(interval);
 		}
 	}, 50);
