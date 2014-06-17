ce0003a57dd22308675ee63946872cb5e02af2f9
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-17 20:37:00 +0300

    about scroll smooth if small screen

--- client/controllers/outro.js
+++ client/controllers/outro.js
@@ -2,7 +2,10 @@ Template.outro.events({
 	'click #about': function(e, tmpl) {
 		if ( $( "#outro-hidden" ).is( ":hidden" ) ) {
 			$( "#outro-hidden" ).css('display', 'flex');
-			$('html,body').animate({scrollTop: $(document).height() - $(window).height()}, 1000);
+			var bottom = $(document).height() - $(window).height();
+			var outroTop = $('.outro').offset().top + 5;
+			var scrolltop = bottom < outroTop ? bottom : outroTop;
+			$('html,body').animate({scrollTop: scrolltop}, 1000);
 		} else {
 			$( "#outro-hidden" ).css('display', 'none');
 		}
