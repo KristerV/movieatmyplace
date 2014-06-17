7457916e195aa3bc97c30daa7e714f28960e980d
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-17 20:15:47 +0300

    save on checkbox change

--- client/controllers/host.js
+++ client/controllers/host.js
@@ -2,7 +2,6 @@ Template.host.helpers({
 	items: function() {
 		var Event = Events.findOne({_id: Session.get('eId')});
 		if (isset(Event) && isset(Event['host'])) {
-			console.log(Event.host);
 			delete Event.host['friendsAddMovies'];
 			delete Event.host['friendsInvite'];
 			return getKeyValuePairs(Event['host']);
@@ -11,11 +10,10 @@ Template.host.helpers({
 });
 
 Template.host.events({
-	'blur input': function(e, tmpl) {
+	'blur input, change input[type=checkbox]': function(e, tmpl) {
 		e.preventDefault();
 		var movies = Events.findOne({_id: Session.get('eId')})['movies'];
 		var formData = getFormData('form[name="hostform"]');
-		console.log(formData);
 		Events.update({_id: Session.get('eId')}, {$set: {host: formData}});
 	},
 });
