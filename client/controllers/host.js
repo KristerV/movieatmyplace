167196e58d318ff11877c60338f6c4199a1bf0c9
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-06-18 00:41:08 +0300

    shitty mailgun implemetation

--- client/controllers/host.js
+++ client/controllers/host.js
@@ -10,10 +10,18 @@ Template.host.helpers({
 });
 
 Template.host.events({
-	'blur input, change input[type=checkbox]': function(e, tmpl) {
+	'blur input, change input[type=checkbox]:not(input[name=email])': function(e, tmpl) {
 		e.preventDefault();
 		var movies = Events.findOne({_id: Session.get('eId')})['movies'];
 		var formData = getFormData('form[name="hostform"]');
+		delete formData.Email;
 		Events.update({_id: Session.get('eId')}, {$set: {host: formData}});
 	},
+	'blur input[name=Email]': function(e, tmpl) {
+		if (!isValidEmailAddress(e.currentTarget.value))
+			return false;
+		
+		Meteor.call('sendEmail', e.currentTarget.value);
+	}
 });
+
