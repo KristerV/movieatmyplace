56f4280d282eaffdc6719698b9c433956ce97d7a
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-10 01:30:15 +0300

    pretty localhost share link

--- client/controllers/share.js
+++ client/controllers/share.js
@@ -2,7 +2,7 @@ Template.share.helpers({
 	sharelink: function() {
 		var link = getDomain()[0];
 		if (link.indexOf("localhost") > -1)
-			link = link + '?eId=' + Session.get('eId');
+			link = 'localhost:3000?eId=' + Session.get('eId');
 		else
 			link = 'movieat.mp/?eId=' + Session.get('eId');
 		return link;
