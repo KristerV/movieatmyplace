649ac9de2086537dc937ccee8c5a686e84708370
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-10 01:28:22 +0300

    localhost share link

--- client/controllers/share.js
+++ client/controllers/share.js
@@ -1,6 +1,10 @@
 Template.share.helpers({
 	sharelink: function() {
-		var link = 'movieat.mp/?eId=' + Session.get('eId');
+		var link = getDomain()[0];
+		if (link.indexOf("localhost") > -1)
+			link = link + '?eId=' + Session.get('eId');
+		else
+			link = 'movieat.mp/?eId=' + Session.get('eId');
 		return link;
 	}
 });
