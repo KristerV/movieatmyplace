eb871435f6a2810de802220b099950a13fef89f1
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-10 16:53:49 +0300

    pure CSS trailer positioning

--- client/controllers/trailer.js
+++ client/controllers/trailer.js
@@ -7,22 +7,15 @@ Template.trailer.helpers({
 Template.trailer.events({
 	'click p': function(e, tmpl) {
 		var node = $('.trailer-container');
-		if ($(document).width() <= 800)
-			node.animate({right: '-100%'}).promise().done(function(){
-				Session.set('youtubeTerms', null);
-			});
-		else
-			node.animate({right: '-50%'}).promise().done(function(){
-				Session.set('youtubeTerms', null);
-			});
+		node.addClass('trailer-hidden').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+			Session.set('youtubeTerms', null);
+		});
 	}
 });
 
 Template.trailer.rendered = function() {
 	var node = $(this.firstNode);
-	node.removeClass('trailer-hidden');
-	if ($(document).width() <= 800)
-		node.css('right', '-100%').animate({right: '0'});
-	else
-		node.css('right', '-50%').animate({right: '0.5em'});
+	Meteor.setTimeout(function(){
+		node.removeClass('trailer-hidden');
+	}, 1);
 }
