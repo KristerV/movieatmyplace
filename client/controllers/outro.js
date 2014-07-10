3c2133ffb0d62f1bd17194733bfb0c7ff21ada70
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-10 10:46:35 +0300

    outro close styling

--- client/controllers/outro.js
+++ client/controllers/outro.js
@@ -1,7 +1,7 @@
 Template.outro.events({
 	'click #about': function(e, tmpl) {
 		if ( $( "#outro-hidden" ).is( ":hidden" ) ) {
-			$( "#outro-hidden" ).css('display', 'flex');
+			$( "#outro-hidden" ).css({'display': 'flex', 'color': 'rgba(0,0,0,0.5)'});
 			var bottom = $(document).height() - $(window).height();
 			var outroTop = $('.outro').offset().top + 5;
 			var scrolltop = bottom < outroTop ? bottom : outroTop;
@@ -10,8 +10,8 @@ Template.outro.events({
 			$('.section:last-child').removeClass("with-background-image");
 			$('.section:last-child').addClass("no-background-image");
 		} else {
-			$('.outro').css("background-color", 'none');
-			$( "#outro-hidden" ).slideUp(1000);
+			$('.outro').css({'background-color': 'none'});
+			$( "#outro-hidden" ).css({'color': 'transparent'}).slideUp(1000);
 			if ($('.backstretch').length > 0) {
 				$('.section:last-child').removeClass("no-background-image");
 				$('.section:last-child').addClass("with-background-image");
