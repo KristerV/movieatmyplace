115399edbb2b79f1235d98757de5e4fe5d8e0060
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-29 22:19:52 +0300

    better outro sliding and shadows

--- client/controllers/outro.js
+++ client/controllers/outro.js
@@ -2,16 +2,17 @@ Template.outro.events({
 	'click #about': function(e, tmpl) {
 		if ( $( "#outro-hidden" ).is( ":hidden" ) ) {
 			$( "#outro-hidden" ).css({'display': 'flex', 'color': 'rgba(0,0,0,0.5)'});
+			$('#outro-hidden a').css({'color': 'rgba(0,0,0,0.5)', 'border-bottom': '1px solid rgba(0,0,0,0.5)'});
 			var bottom = $(document).height() - $(window).height();
 			var outroTop = $('.outro').offset().top + 5;
 			var scrolltop = bottom < outroTop ? bottom : outroTop;
 			$('html,body').animate({scrollTop: scrolltop}, 1000);
-			$('.outro').css("background-color", 'rgba(230,230,230,0.9)');
 			$('.section:last-child').removeClass("with-background-image");
 			$('.section:last-child').addClass("no-background-image");
 		} else {
-			$('.outro').css({'background-color': 'none'});
-			$( "#outro-hidden" ).css({'color': 'transparent'}).slideUp(1000);
+			$( "#outro-hidden" ).css({'color': 'transparent'});
+			$('#outro-hidden a').css({'color': 'transparent', 'border-bottom': '1px solid rgba(0,0,0,0)'});
+			$( "#outro-hidden" ).slideUp(1001);
 			if ($('.backstretch').length > 0) {
 				$('.section:last-child').removeClass("no-background-image");
 				$('.section:last-child').addClass("with-background-image");
